077e45056faea314ef5830d4e608340ea0a2e1b4
William Pitcock <nenolod@dereferenced.org>
2012-10-12 20:44:37 +0000

    m_sasl: add unrealircd SASL implementation

--- m_sasl.cpp
+++ m_sasl.cpp
@@ -5,13 +5,22 @@
 
 #include "module.h"
 
-std::set<Anope::string> sasl_sessions;
+class SASLMechanism;
+
+std::map<Anope::string, SASLMechanism *> sasl_sessions;
 
 class SASLImplementation
 {
+ protected:
+	Module *m_module;
+	SASLImplementation(Module *module) : m_module(module) { };
+
  public:
 	void SendSVSLOGIN(Anope::string target, Anope::string login);
 	void SendSASL(Anope::string target, char mode, Anope::string data);
+	void HandleMessage(Anope::string uid, char mode, Anope::string data);
+	void DestroySession(Anope::string uid);
+	SASLMechanism *FindSession(Anope::string uid, Anope::string mech);
 };
 
 class SASLMechanism;
@@ -36,7 +45,7 @@ class SASLMechanism
 
  public:
 	SASLMechanism(Module *m, SASLImplementation *impl, Anope::string uid) : m_impl(impl), m_uid(uid), m_module(m) { };
-	~SASLMechanism();
+	virtual ~SASLMechanism();
 
 	virtual void MechStart();
 	virtual void MechStep(Anope::string in);
@@ -52,11 +61,13 @@ class SASLMechanism
 	{
 		m_impl->SendSVSLOGIN(m_uid, account);
 		m_impl->SendSASL(m_uid, 'D', "S");
+		m_impl->DestroySession(m_uid);
 	}
 
 	void OnFailedAuth()
 	{
 		m_impl->SendSASL(m_uid, 'D', "F");
+		m_impl->DestroySession(m_uid);
 	}
 };
 
@@ -70,9 +81,10 @@ void SASLIdentifyRequest::OnFail()
 	m_mech->OnFailedAuth();
 }
 
-class PlainMechanism : public SASLMechanism
+struct PlainMechanism : public SASLMechanism
 {
- public:
+	PlainMechanism(Module *module, SASLImplementation *impl, Anope::string uid) : SASLMechanism(module, impl, uid) { };
+
 	void MechStart()
 	{
 		m_impl->SendSASL(m_uid, 'C', "+");
@@ -99,5 +111,65 @@ class PlainMechanism : public SASLMechanism
 
 		/* verify password */
 		VerifyPassword(authcid, password);
+	}
+};
+
+SASLMechanism *SASLImplementation::FindSession(Anope::string uid, Anope::string mech)
+{
+	SASLMechanism *mech_p = sasl_sessions[uid];
+
+	if (mech_p)
+		return mech_p;
+
+	mech_p = new PlainMechanism(m_module, this, uid);
+	mech_p->MechStart();
+	sasl_sessions[uid] = mech_p;
+
+	return mech_p;
+}
+
+void SASLImplementation::HandleMessage(Anope::string uid, char mode, Anope::string data)
+{
+	SASLMechanism *mech = FindSession(uid, "PLAIN");
+
+	mech->MechStep(data);
+}
+
+void SASLImplementation::DestroySession(Anope::string uid)
+{
+	SASLMechanism *mech_p = sasl_sessions[uid];
+
+	if (mech_p)
+		delete mech_p;
+
+	sasl_sessions.erase(uid);
+}
+
+class UnrealSASLImplementation : SASLImplementation
+{
+	struct IRCDMessageSASL : IRCDMessage
+	{
+		SASLImplementation *m_parent;
+
+		IRCDMessageSASL(SASLImplementation *parent) : IRCDMessage("SASL", 3), m_parent(parent) { };
+
+		/*
+		 * params[0] = uid
+		 * params[1] = mode
+		 * params[2] = data
+		 */
+		bool Run(MessageSource &source, const std::vector<Anope::string> &params)
+		{
+			m_parent->HandleMessage(params[0], params[1][0], params[2]);
+			return true;
+		}
+	};
+
+	IRCDMessageSASL *sasl_message;
+
+	UnrealSASLImplementation(Module *module) : SASLImplementation(module)
+	{
+		/* XXX: memory leak on module unload... */
+		sasl_message = new IRCDMessageSASL(this);
 	}
 };
